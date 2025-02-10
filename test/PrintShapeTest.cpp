2ac4565e8ad6f925fc0d9b77660293a6de9f1700
Yong Jin, Cho <drajin.cho@gmail.com>
2025-02-10 12:53:40 +0900

    Initial commit

--- test/PrintShapeTest.cpp
+++ test/PrintShapeTest.cpp
@@ -161,8 +161,3 @@ TEST(PrintShapeTest, BMPShape) {
     EXPECT_EQ(oss.str(), expectedOutput);
 }
 
-// Main function to run all tests
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
